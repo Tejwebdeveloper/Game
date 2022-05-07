ec5a2f295ab5f8dca853e4a8a753a43ba3eca36e
B.Tejaswini <btejaswini538@gmail.com>
2022-05-07 22:35:56 +0530

    initial commit

---
+++ first.js
@@ -0,0 +1,12 @@
+/**var names[]={"Rahul","Teju","Nitin","Sashi"};
+var temp;
+for(int i=0;i<n;i++){
+	for(int j=i+1;j<n;j++){
+		
+	}
+}if(names[i].compareTo(names[j])>0){
+	temp=names[i];
+	names[i]=names[j];
+	names[j]=temp;
+}
+}*/
---
+++ second.js
@@ -0,0 +1,5 @@
+/** var fruits=["Apple","Banana","Mango","Orange","Papaya"];
+
+for(var fruit of fruits){
+document.write(fruit + "<br>");
+}*/
